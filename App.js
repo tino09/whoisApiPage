fd820adf9a922765d89b24c81ed327b4120e842e
Valentine Ayim <valentineayimodu@gmail.com>
2023-09-20 17:31:11 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import AppNavbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+      {/* The rest of your application content */}
+    </div>
+  );
+}
